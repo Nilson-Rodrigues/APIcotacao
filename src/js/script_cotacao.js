99aeff4f0c0fd4cb8cf70b1b05731bbf1bd49810
Francinilson Rodrigues <58186025+Nilson-Rodrigues@users.noreply.github.com>
2023-02-05 20:06:45 -0300

    fist commit

---
+++ src/js/script_cotacao.js
@@ -0,0 +1,156 @@
+const url = 'https://economia.awesomeapi.com.br/last/';
+const coins = 'USD-BRL,EUR-BRL,BTC-BRL';
+
+
+fetch(url + coins).then(function (response) {
+    return response.json()
+}).then(function (data) {
+
+    const btcReal = data.BTCBRL
+
+    //variavel para formatar data para o pais local
+    let estadata = new Date(btcReal.create_date)
+
+    document.getElementById('title').innerHTML = btcReal.name
+    document.getElementById('thisdate').innerHTML = estadata.toLocaleString()
+    document.getElementById('maxvalue').innerHTML = parseFloat(btcReal.high).toLocaleString(
+        'pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    document.getElementById('minvalue').innerHTML = parseFloat(btcReal.low).toLocaleString(
+        'pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    document.querySelector('#compra').innerHTML = parseFloat(btcReal.bid).toLocaleString(
+        'pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        }
+    )
+    document.querySelector('#venda').innerHTML = parseFloat(btcReal.ask).toLocaleString(
+        'pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        }
+    )
+    document.querySelector('#pctVariacao').innerHTML = parseFloat(btcReal.pctChange).toLocaleString()
+
+})
+
+
+function cotacao_EUR_BRL (){
+ 
+    fetch(url + coins).then(function(response){
+        return response.json();
+    }).then(function(data){
+        const euroReal = data.EURBRL;
+
+        estadata = new Date(euroReal.create_date);
+
+        document.querySelector('#title').innerHTML = euroReal.name;
+        document.querySelector('#thisdate').innerHTML = estadata.toLocaleString();
+        document.querySelector('#maxvalue').innerHTML = parseFloat(euroReal.high).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            })
+        document.querySelector('#minvalue').innerHTML = parseFloat(euroReal.low).toLocaleString(
+            'pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        document.querySelector('#compra').innerHTML = parseFloat(euroReal.bid).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#venda').innerHTML = parseFloat(euroReal.ask).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#pctVariacao').innerHTML = parseFloat(euroReal.pctChange).toLocaleString()
+    
+    })
+    //setInterval(cotacao_EUR_USD, 30000)
+}
+function cotacao_USD_BRL(){
+    fetch(url + coins).then(function(response){
+        return response.json();
+    }).then(function(data){
+        const usdBRL = data.USDBRL;
+
+        estadata = new Date(usdBRL.create_date);
+
+        document.querySelector('#title').innerHTML = usdBRL.name;
+        document.querySelector('#thisdate').innerHTML = estadata.toLocaleString();
+        document.querySelector('#maxvalue').innerHTML = parseFloat(usdBRL.high).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#minvalue').innerHTML = parseFloat(usdBRL.low).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#compra').innerHTML = parseFloat(usdBRL.bid).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#venda').innerHTML = parseFloat(usdBRL.ask).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#pctVariacao').innerHTML = parseFloat(usdBRL.pctChange).toLocaleString()
+    
+    })
+}
+function cotacao_BTC_BRL(){
+
+    fetch(url + coins).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+    
+        const btcReal = data.BTCBRL
+    
+        //variavel para formatar data para o pais local
+        let estadata = new Date(btcReal.create_date)
+    
+        document.getElementById('title').innerHTML = btcReal.name
+        document.getElementById('thisdate').innerHTML = estadata.toLocaleString()
+        document.getElementById('maxvalue').innerHTML = parseFloat(btcReal.high).toLocaleString(
+            'pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        document.getElementById('minvalue').innerHTML = parseFloat(btcReal.low).toLocaleString(
+            'pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        document.querySelector('#compra').innerHTML = parseFloat(btcReal.bid).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#venda').innerHTML = parseFloat(btcReal.ask).toLocaleString(
+            'pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        )
+        document.querySelector('#pctVariacao').innerHTML = parseFloat(btcReal.pctChange).toLocaleString()
+    })
+}
